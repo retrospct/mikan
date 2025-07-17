5a5c8e014ed351b54de08890f431061aec298adf
Justin Lee <me@jlee.cool>
2025-07-16 23:56:57 -0700

    feat: adds roboto-flex font family
    - Configures tailwind to include the roboto-flex font family for use in the application.
    - This provides a new, modern font option for improved typography.

--- apps/desktop/tailwind.config.js
+++ apps/desktop/tailwind.config.js
@@ -1,11 +1,16 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './src/renderer/src/**/*.{js,ts,jsx,tsx}',
     './src/renderer/index.html',
     '../../packages/ui/src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'roboto-flex': ['Roboto Flex', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
